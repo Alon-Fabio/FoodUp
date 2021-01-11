2277dd9d1beea7a48e3ee2dc875a7ff9f4091fc0
Alon-Fabio <alon.the.fabio@gmail.com>
2021-01-11 17:26:43 +0200

    SearchBar component build plus clear option

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,59 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ searchVal, onSearchChange, onSearchSubmit }) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={style.SBView}>
+      <FontAwesome name="search" size={35} style={style.SBIcon} />
+      <TextInput
+        style={style.SBInput}
+        value={searchVal}
+        placeholder="Search"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={onSearchChange}
+        onEndEditing={() => onSearchSubmit(searchVal)}
+      />
+      {searchVal ? (
+        <TouchableOpacity
+          style={style.SBClear}
+          onPress={() => onSearchChange("")}
+        >
+          <Text style={style.SBClear}>&Chi;</Text>
+        </TouchableOpacity>
+      ) : null}
     </View>
   );
 };
+const style = StyleSheet.create({
+  SBView: {
+    flexDirection: "row",
+    backgroundColor: "#F0EEEE",
+    borderRadius: 5,
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  SBIcon: {
+    alignSelf: "center",
+    marginHorizontal: 5,
+  },
+  SBInput: {
+    height: 50,
+    fontSize: 18,
+    flex: 1,
+  },
+  SBClear: {
+    fontSize: 30,
+    alignSelf: "center",
+    marginHorizontal: 5,
+  },
+});
 
 export default SearchBar;
-
-const styles = StyleSheet.create({});
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [searchVal, setSearchVal] = useState("");
   return (
-    <View>
+    <View style={style.SSView}>
+      <SearchBar
+        searchVal={searchVal}
+        onSearchChange={setSearchVal}
+        onSearchSubmit={(value) => console.log(value)}
+      />
       <Text>Screen Search</Text>
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  SSView: {
+    backgroundColor: "#fff",
+  },
+});
 
 export default SearchScreen;
