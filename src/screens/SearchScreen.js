2730481ded9d68cb8f021defb0a3453a6d249924
Alon-Fabio <alon.the.fabio@gmail.com>
2021-01-12 11:32:09 +0200

    Added API folder with yelp api configed for search & added fetch call to SearchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../API/yelp";
 
 const SearchScreen = () => {
   const [searchVal, setSearchVal] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    if (searchVal === "" && restaurants.length === 0 && errorMsg === "") {
+      fetchResYelp("pasta");
+    }
+    return () => {
+      setErrorMsg("");
+    };
+  }, []);
+
+  const fetchResYelp = async (PrTerm) => {
+    try {
+      const yelpObj = await yelp.get("/search", {
+        params: {
+          term: PrTerm,
+          limit: "50",
+          location: "san jose",
+        },
+      });
+      setRestaurants(yelpObj.data.businesses);
+    } catch {
+      setErrorMsg("Ops... the search failed, please try again later.");
+    }
+  };
+
   return (
     <View style={style.SSView}>
       <SearchBar
         searchVal={searchVal}
         onSearchChange={setSearchVal}
-        onSearchSubmit={(value) => console.log(value)}
+        onSearchSubmit={() => fetchResYelp(searchVal)}
       />
-      <Text>Screen Search</Text>
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
+      <Text>We found {restaurants.length} in your area!</Text>
     </View>
   );
 };
